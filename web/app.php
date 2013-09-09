c11a75309c009bbc9fc59bc6b37a0283becd3e85
maximebf <maxime.bouroumeau@gmail.com>
2013-09-09 13:46:42 -0400

    dashboard: refactored action, added behaviors, bug fix

--- web/app.php
+++ web/app.php
@@ -2,6 +2,11 @@
 
 require_once("../vendor/autoload.php");
 
+require '/home/ubuntu/maxime/tmp/vendor/autoload.php';
+
+set_include_path("/home/ubuntu/maxime/tmp/build/classes" . PATH_SEPARATOR . get_include_path());
+Propel::init('/home/ubuntu/maxime/tmp/build/conf/demo-conf.php');
+
 include __DIR__ . '/../demo/ApplicationKernel.php';
 
 $application = ApplicationKernel::createInstance();
