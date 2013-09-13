62eb7d9b37713e888fb4dc915e0107d5a9f46a55
mpoiriert <mpoiriert@gmail.com>
2013-09-13 13:16:46 -0400

    [+] front controller exception handler system

---
+++ src/Nucleus/FrontController/EventDispatchingExceptionHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Nucleus\FrontController;
+
+use Nucleus\IService\FrontController\IExceptionHandler;
+use Nucleus\IService\EventDispatcher\IEventDispatcherService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Exception;
+
+/**
+ * Description of EventDispatchingExceptionHandler
+ *
+ * @author Martin
+ */
+class EventDispatchingExceptionHandler implements IExceptionHandler
+{
+    private $eventDispatcher;
+    
+    /**
+     * @\Nucleus\IService\DependencyInjection\Inject
+     * 
+     * @param IEventDispatcherService $eventDispatcher
+     */
+    public function __construct(IEventDispatcherService $eventDispatcher)
+    {
+       $this->eventDispatcher = $eventDispatcher; 
+    }
+
+    public function handleException(Exception $exception, Request $request, Response $response)
+    {
+        $this->eventDispatcher->dispatch(
+            'Exception.thrownFromFrontController',
+            $exception,
+            array('request'=>$request,'response'=>$response)
+        );
+    }
+}
